39bc5b5109e6f5a4239d3980ae422e46f41e37ab
Marcus Dantas <marvinsd@gmail.com>
2026-02-13 22:28:13 -0300

    chore: add initial server config

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,33 @@
-console.log("Hello, World!");
+import { createServer } from "net";
+
+import type { serverConfig } from "./types.js";
+import type { Server, Socket } from "net";
+
+function handleConnection(connection: Socket) {
+  console.log("Client connected");
+
+  connection.on("data", (data) => {
+    console.log(`Received: ${data.toString()}`);
+  });
+
+  connection.on("close", () => {
+    console.log("Client disconnected");
+  });
+}
+
+function main() {
+  const config: serverConfig = {
+    host: "127.0.0.1",
+    port: 6379,
+  };
+
+  const server: Server = createServer();
+  server.listen(config.port, config.host);
+  console.log(`Listening on ${config.host}:${config.port.toString()}`);
+
+  server.on("connection", (connection) => {
+    handleConnection(connection);
+  });
+}
+
+main();
---
+++ src/types.ts
@@ -0,0 +1,4 @@
+export interface serverConfig {
+  host: string;
+  port: number;
+}
