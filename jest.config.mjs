781aa288b69079c4f9d8906d0889d1b07c69b596
Marcus Dantas <marvinsd@gmail.com>
2026-06-14 19:09:34 -0300

    chore: add jest

---
+++ jest.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('jest').Config} */
+const config = {
+  testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  transform: {
+    "^.+\\.ts$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
+  },
+};
+
+export default config;
